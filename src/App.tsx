2950151f2497d10d47ac3c8bb00a1730e85d9536
spring <dongwon1826@gmail.com>
2022-06-23 21:42:30 +0900

    chore: add lint, emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/home";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./style";
+
+interface HomeProps {}
+
+const Home: React.FC<HomeProps> = (props) => {
+  return <S.Wrappwe>Home</S.Wrappwe>;
+};
+
+export default Home;
